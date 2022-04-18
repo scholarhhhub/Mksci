30c7deddfaf047e2730354da8ac9c1bb1600ac7c
咩咩 <1064381418@qq.com>
2022-04-18 17:15:59 +0800

    format code

--- commands/project.py
+++ commands/project.py
@@ -1,57 +1,74 @@
-from shutil import copyfile
 import logging
 import os
+from shutil import copyfile
 
 
 def new(output_dir=""):
-    log_path=os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))),'log')
-    logging.basicConfig(filename=os.path.join(log_path,'new.log'), format='%(asctime)s - %(name)s - %(levelname)s -%(module)s:  %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S ',
-                        level=logging.INFO)
+    log_path = os.path.join(
+        os.path.dirname(os.path.abspath(os.path.dirname(__file__))), "log"
+    )
+    logging.basicConfig(
+        filename=os.path.join(log_path, "new.log"),
+        format="%(asctime)s - %(name)s - %(levelname)s -%(module)s:  %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S ",
+        level=logging.INFO,
+    )
     logger = logging.getLogger()
     KZT = logging.StreamHandler()
     KZT.setLevel(logging.DEBUG)
     logger.addHandler(KZT)
 
     dir_path = os.getcwd()
-    dirname=output_dir
-    output_dir=os.path.join(dir_path,dirname)
-    mksci_config=os.path.join(dir_path,'.mksci')
-    if len(output_dir) > 1 :
+    dirname = output_dir
+    output_dir = os.path.join(dir_path, dirname)
+    mksci_config = os.path.join(dir_path, ".mksci")
+    if len(output_dir) > 1:
         if not os.path.exists(output_dir):
             logger.info(f"Creating project directory: {output_dir}")
             os.mkdir(output_dir)
             os.mkdir(mksci_config)
             config_path = os.path.join(output_dir, "config.yaml")
-            template_path=os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))),'template.yaml')
+            template_path = os.path.join(
+                os.path.dirname(os.path.abspath(os.path.dirname(__file__))),
+                "template.yaml",
+            )
             logger.info(f"Writing initial config yaml: {config_path}")
             # copyfile("../template.yaml",config_path)
-            copyfile(template_path,config_path)
+            copyfile(template_path, config_path)
         else:
             logger.error(f"Project {output_dir} already exists.")
             # return
     else:
         logger.error("Please enter project name.")
 
-    docs_path = os.path.join(output_dir, 'docs')
+    docs_path = os.path.join(output_dir, "docs")
     if not os.path.exists(docs_path):
         os.mkdir(docs_path)
     else:
         None
-        
+
+
 def init():
-    log_path=os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))),'log')
-    logging.basicConfig(filename=os.path.join(log_path,'init.log'), format='%(asctime)s - %(name)s - %(levelname)s -%(module)s:  %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S ',
-                        level=logging.INFO)
+    log_path = os.path.join(
+        os.path.dirname(os.path.abspath(os.path.dirname(__file__))), "log"
+    )
+    logging.basicConfig(
+        filename=os.path.join(log_path, "init.log"),
+        format="%(asctime)s - %(name)s - %(levelname)s -%(module)s:  %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S ",
+        level=logging.INFO,
+    )
     logger = logging.getLogger()
     KZT = logging.StreamHandler()
     KZT.setLevel(logging.DEBUG)
     logger.addHandler(KZT)
-    dir_path=os.getcwd()
+    dir_path = os.getcwd()
     config_path = os.path.join(dir_path, "config.yaml")
-    mksci_config=os.path.join(dir_path,".mksci")
-    template_path=os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))),'template.yaml')
+    mksci_config = os.path.join(dir_path, ".mksci")
+    template_path = os.path.join(
+        os.path.dirname(os.path.abspath(os.path.dirname(__file__))),
+        "template.yaml",
+    )
     if not os.path.exists(mksci_config):
         os.mkdir(mksci_config)
     else:
@@ -60,14 +77,15 @@ def init():
         logger.error(f"Project {dir_path} already exists.")
     else:
         logger.info(f"Writing initial config yaml: {config_path}")
-        copyfile(template_path,config_path)
+        copyfile(template_path, config_path)
 
-    docs_path = os.path.join(dir_path, 'docs')
+    docs_path = os.path.join(dir_path, "docs")
     if not os.path.exists(docs_path):
         os.mkdir(docs_path)
     else:
         None
 
-#测试代码
+
+# 测试代码
 # new("testproject")
 init()
--- commands/refresh.py
+++ commands/refresh.py
@@ -1,32 +1,36 @@
 import os
 import uuid
+
 import config
 
-all_files=[]
-def refresh(path,file,config_name):
-    configs=config.getConfig(config_name)
-    filename=os.path.basename(file)
+all_files = []
+
+
+def refresh(path, file, config_name):
+    configs = config.getConfig(config_name)
+    filename = os.path.basename(file)
     suffix = filename.split(".")[1]
-    filename=filename.split(".")[0]
-    newfile=filename+'_'+str(uuid.uuid1())[:13]+'.'+suffix
-    generated_path=os.path.join(path,'generated')
+    filename = filename.split(".")[0]
+    newfile = filename + "_" + str(uuid.uuid1())[:13] + "." + suffix
+    generated_path = os.path.join(path, "generated")
     if not os.path.exists(generated_path):
         os.mkdir(generated_path)
     else:
         None
-    newfile_path=os.path.join(generated_path,newfile)
-    print(generated_path,newfile_path)
+    newfile_path = os.path.join(generated_path, newfile)
+    print(generated_path, newfile_path)
     # print(configs)
-    with open(file, 'r', encoding='utf-8') as f:
-        text=f.read()
+    with open(file, "r", encoding="utf-8") as f:
+        text = f.read()
     # print(configs)
     for ck in configs.keys():
-        cv=str(configs.get(ck))
+        cv = str(configs.get(ck))
         # print(ck,cv)
-        pattern='{%'+ck+'%}'
-        text=text.replace(pattern,cv)
-    with open(newfile_path, 'w',encoding='utf-8') as f:
+        pattern = "{%" + ck + "%}"
+        text = text.replace(pattern, cv)
+    with open(newfile_path, "w", encoding="utf-8") as f:
         f.write(text)
+
 
 def getFiles(path):
     if not os.path.isdir(path):
@@ -34,18 +38,19 @@ def getFiles(path):
     else:
         file_list = os.listdir(path)
         for file in file_list:
-            file_path=os.path.join(path,file)
+            file_path = os.path.join(path, file)
             if os.path.isdir(file_path):
                 getFiles(file_path)
             else:
                 all_files.append(file_path)
 
-#测试代码
-dir_path=os.path.abspath(os.path.dirname(__file__))
-docs_path=os.path.join(dir_path,'docs')
-config_path=os.path.join(dir_path,'config.yaml')
+
+# 测试代码
+dir_path = os.path.abspath(os.path.dirname(__file__))
+docs_path = os.path.join(dir_path, "docs")
+config_path = os.path.join(dir_path, "config.yaml")
 getFiles(docs_path)
 print(all_files)
 for file in all_files:
-    if not 'generated' in file:
-        refresh(docs_path,file,config_path)
+    if not "generated" in file:
+        refresh(docs_path, file, config_path)
